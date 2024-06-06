236d0d8eb3d231efd1ff6ad2ef3a1a50b9ee79ed
Samuel Vieira <vieirasamuel000@gmail.com>
2024-06-05 23:18:37 -0300

    adiciona os arquivos do projeto

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function ativarContagem() {
+    tempo = setInterval(function () {
+        const horas = document.getElementById('horas').innerHTML;
+        const minutos = document.getElementById('minutos').innerHTML;
+        const segundos = document.getElementById('segundos').innerHTML;
+        
+        let hora = parseInt(horas) + 1;
+        let minuto = parseInt(minutos) + 1;
+        let segundo = parseInt(cronometro) + 1;
+
+        document.getElementById('segundos').innerHTML = segundo;
+        document.getElementById('display').innerHTML = 'Iniciado';
+
+        if (segundo == 60) {
+            document.getElementById('segundos').innerHTML = 0;
+            document.getElementById('minutos').innerHTML = minuto;
+            if (minuto == 60) {
+                document.getElementById('minutos').innerHTML = 0;
+                document.getElementById('horas').innerHTML = hora;
+            }
+        }
+    }, 1000);
+}
+
+function pararContagem() {
+    clearInterval(tempo);
+    document.getElementById('display').innerHTML = 'Parou';
+}
+
+function zerarContagem() {
+    document.getElementById('horas').innerHTML = 0;
+    document.getElementById('minutos').innerHTML = 0;
+    document.getElementById('segundos').innerHTML = 0;
+    document.getElementById('display').innerHTML = '';
+}
